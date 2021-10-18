865b182fab3a40a1a738d8eef6cd54b36b438542
Alpha Diallo <73855044+AlphaOmarDiallo@users.noreply.github.com>
2021-10-18 09:38:38 +0200

    Final

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -1,24 +1,20 @@
 package com.openclassrooms.magicgithub.repository;
 
 import com.openclassrooms.magicgithub.api.ApiService;
-// import com.openclassrooms.magicgithub.api.FakeApiService;
-// import com.openclassrooms.magicgithub.api.FakeApiServiceGenerator;
+import com.openclassrooms.magicgithub.api.FakeApiService;
+import com.openclassrooms.magicgithub.api.FakeApiServiceGenerator;
 import com.openclassrooms.magicgithub.model.User;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
 
-    private final ApiService apiService; // TODO: A utiliser
+    private final ApiService apiService;
 
     public UserRepository(ApiService apiService) {
         this.apiService = apiService;
     }
-
-    /* Implemented methods from FakeApiService as UserRepository acts as a liaison between the mobile
-    app user and the Api as they should not interact directly.
-    Used apiService as a variable to target the methods from ApiService as they are not static.*/
 
     public List<User> getUsers() {
         // TODO: A modifier
